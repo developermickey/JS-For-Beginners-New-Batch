7b23dec511bb406acf6ccd70be2953b2ae1a3c89
developermickey <mukesh.quazma@gmail.com>
2024-10-01 12:05:30 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,110 @@
+// console.log("He")
+
+// Single Line Comment  shorcut ctrl + /
+// dfdhsjkfdhsfgs
+
+// Multiline comment shift + alt + A
+
+/* 
+dhfgklfhgk
+hjhjgnfkgl    
+ djkfghdjkgds
+hjlghjdflkg
+*/
+
+// Variable Kya hote hai
+
+// kisi bhi data ko store karne ke liye ke cheez chahiye jo ki hoti variable
+
+//  variable hai wo ek container hai jisme hum koi bhi value add kar sakte hai
+
+// keyword  container name = value
+
+// var name = "Mayur";
+// var name = "Mukesh";
+
+// var let const
+
+// let name = "Mayur";
+// name = "Mukesh";
+
+// const - jiski value change nahi kar sakte
+
+// const PI = 3.14;
+// PI = 1.14;
+
+// Datatypes
+// 2 Type of datatype in javascript
+
+// 1. Primitive Datatype
+
+// String - Sare text "fhjkdshfkdjs" 'a'
+// Number - Sare Number 121, 10.010, 1.12123131213
+// Boolean - true / false
+// Null - let a = null;
+// Undefined - let a;
+// BigInt
+// Symbol
+
+// 2. Non Primitive Datatype / Reference Type
+
+// () Method
+// {} Object
+// [] Array
+
+// String - Sare text
+
+let myName = "Mayur";
+let myChar = "M";
+
+// Number - sare number 0 to 9
+
+let age = 32;
+let itemPrice = 1999.12;
+
+// Boolean - true and false
+
+let isStudentPass = true;
+let isStudentFail = false;
+
+// Null
+
+let user = null;
+
+// Undefined
+
+let a;
+
+// () method
+
+// { } Object
+
+// {
+//     key: value,
+// }
+
+let studentInfo = {
+  name: "Mayur",
+  age: 32,
+  phone: 7878787878,
+  isPass: true,
+  address: {
+    ward: 14,
+    houseNo: 1104,
+    steet: "Bhai Lal Colony",
+    city: "Nagpur",
+    state: "M.P.",
+  },
+  class: 8,
+};
+
+// console.log(studentInfo.address.steet);
+
+// Array
+
+let marks = [25, 35, 78, 99, 100, 98];
+//  index =  0    1   2   3   4   5
+
+console.log(marks[0]);
+
+// console.log(typeof marks); // datatype ko dekh sakte hai typeof
