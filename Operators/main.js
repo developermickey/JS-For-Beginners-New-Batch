98e6c4d71d1a80434e9cca8b810f5c7e36da5f75
developermickey <mukesh.quazma@gmail.com>
2024-10-02 12:02:42 +0530

    JavaScript Operators

---
+++ Operators/main.js
@@ -0,0 +1,136 @@
+// JavaScript Operators
+
+/* 
+The Assignment Operator = assigns values
+
+The Addition Operator + adds values 
+
+The Multiplication Operator * multiplies values
+
+The Comparison Operator > compares values
+
+*/
+
+// Assignment Operator  =
+
+let age = 32;
+
+// Addition Operator +
+let xx = 5;
+let yy = 2;
+let zz = xx + yy; // 7
+
+// JavaScript Multiplication *
+let x = 5;
+let y = 2;
+let z = x * y; // 10
+
+/* 
+Arithmetic Operators 
+
+/* 
+
++	Addition
+-	Subtraction
+*	Multiplication
+**	Exponentiation Power 2**3 (ES2016)
+// console.log(2 ** 3);
+
+/	Division
+%	Modulus (Division Remainder)
+console.log(3 % 2);
+++	Increment 
+
+prefix
+
+let a = 5;
+
+// prefix value pahle update ho jayegi
+// a = 1 + a
+// console.log(++a);
+
+// postfix value bad me update hogi
+console.log(a++);
+// a = a + 1
+
+
+
+
+--	Decrement postfix , prefix
+
+*/
+
+/*
+Assignment Operators
+
+Operator	Example	Same As  x = 5 y = 10
+=	     x = y	     x = y
++=	     x += y	     x = x + y  x = 5 + 10
+-=	     x -= y	     x = x - y
+*=	     x *= y	     x = x * y
+/=	     x /= y	     x = x / y
+%=	     x %= y	     x = x % y
+**=	     x **= y	x = x ** y
+
+Comparison     Operators
+==	   datatype ko check nahi karta          equal to check karte hia samne wale value meri value equal h yeah nahi 
+===	            equal value and equal type
+!=	            not equal
+!==	            not equal value or not equal type
+>	            greater than
+<	            less than
+>=	            greater than or equal to
+<=	            less than or equal to
+?               Ternary
+
+
+// let n = 2;
+// let o = 3;
+
+// n > o ? console.log("Yes") : console.log("No");
+
+Ternary Operators
+
+Logical Operators
+
+AND, &&  2 conditions hogi wo 2no hib true hona chahiye 
+exam 
+1. driving aana jaruri hai
+2. 18+ 
+let myAge = 18;
+let isDriving = true;
+
+if (myAge >= 18 && isDriving == true) {
+  console.log("You are eligiable for lis");
+} else {
+  console.log("You are not eligiable for lis");
+}
+
+OR || 2 conditon me se koi ek bhi true hogi to hum true milega
+exam 
+1. 75 %
+or 
+2. exam fees 
+
+let atte = 74;
+let fees = true;
+
+if (atte >= 75 || fees == true) {
+  console.log("You are eligiable for exam");
+} else {
+  console.log("You are not eligiable for exam");
+}
+
+NOT ! true -> false 
+      false -> true
+
+      let isStudentFail = true;
+
+if (!isStudentFail) {
+  console.log("Yes your fail");
+} else {
+  console.log("Yes your pass");
+}
+
+
+*/
